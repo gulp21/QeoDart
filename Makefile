gulp21/QeoDart

commit fe3276ecaec2b31acf7e3f5f289768ceb1baa7ef

history pointer, ad
* placesHistory is pointer
* readQcf:
** warn on x/y <0 or >600
* fixed coordinates if Sofia
* readme: supported/tested platforms
* basic banner ad

cpp/dart.cpp
@@ -28,7 +28,7 @@ dart::dart(QMainWindow *parent) : QMainWindow(parent) {
 	dPxToKm=1;
 	iCurrentQcf=0; // TODO allow saving name;
 	iScoreAreaMode=1;
-	iTrainingPlaceNumber=-1;
+	pTrainingPlaceNumber=NULL;
 	iPaddingTop=0;
 	iMarginTop=0;
 	
@@ -517,6 +517,8 @@ void dart::resizeEvent(QResizeEvent *event) {
 	vRepaintMap();
 	
 	gridLayoutWidget->setGeometry(QRect(0,0,600*dZoomFactor+1,iPaddingTop+50));
+	
+	lblAd->setGeometry(0,iPaddingTop+600*dZoomFactor,600*dZoomFactor,100);
 
 	qDebug() << "[i] iPaddingTop" << iPaddingTop << "iMarginTop" << iMarginTop << "dZoomFactor" << dZoomFactor << "fontSize" << iGetFontSize();
 	
@@ -698,7 +700,7 @@ void dart::vMouseClickEvent(int x, int y) {
 	score.x=x;
 	score.y=y;
 	score.diffPxArea=dGetDistanceInPx(x,y,iPlaceCount-1); // respects area // TODO what about shown distance?
-	score.diffPx=dGetDistanceInPxBetween(x,y,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->x,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->y);
+	score.diffPx=dGetDistanceInPxBetween(x,y,qlPlacesHistory[iPlaceCount-1]->x,qlPlacesHistory[iPlaceCount-1]->y);
 	score.diffKm=dGetDistanceInKm(score.diffPxArea);
 	score.mark=dGetMarkFromDistance(score.diffPxArea);
 	score.score=dGetScore(score.mark);
@@ -782,7 +784,7 @@ void dart::vShowResultWindows() {
 	bAcceptingClickEvent=FALSE;
 	
 	for(int i=0,max=qlPlacesHistory.count(); i<max; i++) {
-		vDrawPoint(qlCurrentTypePlaces[qlPlacesHistory[i]]->x, qlCurrentTypePlaces[qlPlacesHistory[i]]->y, qlPointLabels, qlCurrentTypePlaces[qlPlacesHistory[i]]->name);
+		vDrawPoint(qlPlacesHistory[i]->x, qlPlacesHistory[i]->y, qlPointLabels, qlPlacesHistory[i]->name);
 	}
 	
 	for(int i=0; i<iNumberOfPlayers; i++) {
@@ -800,8 +802,8 @@ void dart::vResetScoreLabels() {
 }
 
 void dart::vShowCurrentPlace() {
-	if(iAskForMode==enPositions) vDrawPoint(qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->x,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->y,qlPointLabels);
-	else vDrawPoint(qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->x,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->y,qlPointLabels, qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->name);
+	if(iAskForMode==enPositions) vDrawPoint(qlPlacesHistory[iPlaceCount-1]->x,qlPlacesHistory[iPlaceCount-1]->y,qlPointLabels);
+	else vDrawPoint(qlPlacesHistory[iPlaceCount-1]->x,qlPlacesHistory[iPlaceCount-1]->y,qlPointLabels, qlPlacesHistory[iPlaceCount-1]->name);
 }
 
 void dart::vShowScores() {
@@ -1111,21 +1113,22 @@ void dart::vNextRound() {
 	}
 	
 	int pn=-1;
+	place *ppn=NULL;
 	
 	if(iGameMode==enTraining && iPlaceCount>=5) {
 		qDebug() << "Revise";
 		for(int i=0; i<qlScoreHistory[0].count() && pn==-1; i++) {
 			qDebug() << qlScoreHistory[0][i].mark << qlTotalScores[0].mark;
 			if(qlScoreHistory[0][i].mark>=4 || (qlScoreHistory[0][i].mark>2 && qlScoreHistory[0][i].mark>qlTotalScores[0].mark) ) {
-				qDebug() << qlCurrentTypePlaces[qlPlacesHistory[i]]->name;
+				qDebug() << qlPlacesHistory[i]->name;
 				
 				if(iPlaceCount==5 && i==4) {
 					//we shouldn't ask for the last place immediatly
 					//so we keep it in mind
-					iTrainingPlaceNumber=qlPlacesHistory[i];
+					pTrainingPlaceNumber=qlPlacesHistory[i];
 				} else {
 					qlScoreHistory[0][i].mark=0;
-					pn=qlPlacesHistory[i];
+					ppn=qlPlacesHistory[i];
 				}
 			} //if (badscore)
 		} // for (scorehistory)
@@ -1134,31 +1137,33 @@ void dart::vNextRound() {
 		}
 	}
 	
-	if(pn<=-1) {
+	if(pn<=-1 && ppn==NULL) {
 		int i=0;
 		do {
 			qDebug() << qlCurrentTypePlaces.count();
 			pn = rand() % qlCurrentTypePlaces.count();
-		} while(qlPlacesHistory.contains(pn) && i++<10);
+		} while(qlPlacesHistory.contains(qlCurrentTypePlaces[pn]) && i++<10);
 		
-		if(iGameMode==enTraining && iTrainingPlaceNumber!=-1 && iPlaceCount==2) {
-			pn=iTrainingPlaceNumber;
-			iTrainingPlaceNumber=-1;
+		if(iGameMode==enTraining && pTrainingPlaceNumber!=NULL && iPlaceCount==2) {
+			ppn=pTrainingPlaceNumber;
+			pTrainingPlaceNumber=NULL;
+		} else {
+			ppn=qlCurrentTypePlaces[pn];
 		}
 	}
 	
 	iPlaceCount++;
-	qlPlacesHistory.append(pn);
+	qlPlacesHistory.append(ppn);
 	
-	qDebug() << "[i] next place:" << pn << qlCurrentTypePlaces[pn]->name << iPlaceCount << "/" << iMaxPlaceCount;
+	qDebug() << "[i] next place:" << pn << qlPlacesHistory[iPlaceCount-1]->name << iPlaceCount << "/" << iMaxPlaceCount;
 	
 	switch(iAskForMode) {
 		case enPositions:
 			bAcceptingClickEvent=TRUE;
-			lblCurrentPlace->setText(qlCurrentTypePlaces[pn]->name);
+			lblCurrentPlace->setText(qlPlacesHistory[iPlaceCount-1]->name);
 			break;
 		case enNames:
-			vDrawPoint(qlCurrentTypePlaces[pn]->x, qlCurrentTypePlaces[pn]->y, qlPointLabels);
+			vDrawPoint(qlPlacesHistory[iPlaceCount-1]->x, qlPlacesHistory[iPlaceCount-1]->y, qlPointLabels);
 			lineEdit->clear();
 			lineEdit->setStyleSheet("");
 			lineEdit->setEnabled(TRUE);
@@ -1187,13 +1192,13 @@ double dart::dGetDistanceInPxBetween(int a, int b, int x, int y) {
 double dart::dGetDistanceInPx(int a, int b, int n) {
 	if(a==-1 && b==-1) return -1;
 	
-	int x=qlCurrentTypePlaces[qlPlacesHistory[n]]->x;
-	int y=qlCurrentTypePlaces[qlPlacesHistory[n]]->y;
+	int x=qlPlacesHistory[n]->x;
+	int y=qlPlacesHistory[n]->y;
 	
 	if(iAskForMode==enNames) return dGetDistanceInPxBetween(a,b,x,y);
 	
-	int dim2x=qlCurrentTypePlaces[qlPlacesHistory[n]]->dimx*(iScoreAreaMode/2.0)/2;
-	int dim2y=qlCurrentTypePlaces[qlPlacesHistory[n]]->dimx*(iScoreAreaMode/2.0)/2;
+	int dim2x=qlPlacesHistory[n]->dimx*(iScoreAreaMode/2.0)/2;
+	int dim2y=qlPlacesHistory[n]->dimx*(iScoreAreaMode/2.0)/2;
 	
 	if(a>x+dim2x) a-=dim2x;
 	else if(a<x-dim2x) a+=dim2x;
@@ -1370,7 +1375,7 @@ void dart::vReturnPressedEvent() { // TODO split (net!)
 	score.x=x;
 	score.y=y;
 	score.diffPxArea=dGetDistanceInPx(x,y,iPlaceCount-1); // respects area // TODO what about shown distance?
-	score.diffPx=dGetDistanceInPxBetween(x,y,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->x,qlCurrentTypePlaces[qlPlacesHistory[iPlaceCount-1]]->y);
+	score.diffPx=dGetDistanceInPxBetween(x,y,qlPlacesHistory[iPlaceCount-1]->x,qlPlacesHistory[iPlaceCount-1]->y);
 	score.diffKm=dGetDistanceInKm(score.diffPxArea);
 	score.mark=dGetMarkFromDistance(score.diffPxArea);
         score.score=dGetScore(score.mark)*f;

README
@@ -6,6 +6,12 @@ Requirements:
 Download:
 * git: git clone git://github.com/gulp21/QeoDart.git
 
+Supported Operating Systems:
+* Linux (tested Kubuntu 11.04 32/64 bit)
+* Windows (tested Windows 7 32 bit)
+* Windows CE (tested Windows CE Core 5.0)
+* MacOSX is also supported by Qt, but is untested
+
 Support:
 * https://github.com/gulp21/QeoDart/wiki
 * support.gulp21 (ä) googlemail.com

TODO
@@ -1,14 +1,17 @@
+highscore: date (today), time (x ago, api?), pref lang
+Subset > [ ] 1-10; [ ] 11-20; bezogen auf gewhälte Orte
+debug-findplace w/ rectangle [check everything];
+rectangle(x,y,a); rectangle(m1,m2,a,b); rectangle(quadrant)
+small screen: rm "place [x of y]", "points"
+
+
 http://la.wikipedia.org/wiki/Index_civitatum_Europaearum
 http://de.wikipedia.org/wiki/Liste_der_Gro%C3%9Fst%C3%A4dte_in_Deutschland
 http://de.wikipedia.org/wiki/Liste_der_Millionenst%C3%A4dte
 capitals europa
 indien
-history pointer
 git update-index --assume-unchanged
 Ad
-
-[Highscores]
-map1.1="name|score|placecount|time|areamode|…"
 
 //TODOs
 

cpp/dart.hpp
@@ -91,7 +91,7 @@ Q_OBJECT
 		QList<qcfFile> qlQcfxFiles;
 		QList<place> qlAllPlaces; //contains all places of the current map
 		QList<place*> qlCurrentTypePlaces; //contains pointers pointing to the places in qlAllPlaces which fit the current placetype
-		QList<int> qlPlacesHistory;
+		QList<place*> qlPlacesHistory;
 		QList<QList<scoreHistory> > qlScoreHistory; // [player][round].scores
 		QList<totalScore> qlTotalScores;
 		QList<QLabel*> qlPointLabels;
@@ -131,7 +131,9 @@ Q_OBJECT
 		
 		bool bAcceptingClickEvent, bAcceptingResizeEvent;
 		
-		int iCurrentPlayer, iTrainingPlaceNumber, iTimerElapsed;
+		int iCurrentPlayer, iTimerElapsed;
+		
+		place *pTrainingPlaceNumber;
                 
 		QList<QList<QLabel*> > qlCircleLabels; //contains all circles (incl. points) for each user
 // 		QList<QLabel*> qlPlayerLabel;

cpp/io.cpp
@@ -219,8 +219,11 @@ int io::iReadQcf(QString mapname) {
 				if(newPlace.x==-1 || newPlace.y==-1) {
 					qDebug() << "[W] place" << myDart->qlAllPlaces.count() << "has incomplete coordinates";
 				}
+				if(newPlace.x>600 || newPlace.y>600 || newPlace.x<-1 || newPlace.y<-1) {
+					qDebug() << "[W] place" << myDart->qlAllPlaces.count() << "has at least one coordinate out of range";
+				}
 				
-				if(newPlace.placeType.contains("state")) myDart->actionStates->setVisible(TRUE); // TODO count + show count
+				if(newPlace.placeType.contains("state")) myDart->actionStates->setVisible(TRUE); // TODO count + show count; actiongroup caption with total count? [x of y]
 				if(newPlace.placeType.contains("capitalOfState")) myDart->actionCapitals_of_States->setVisible(TRUE);
 				if(newPlace.placeType.contains("country")) myDart->actionCountries->setVisible(TRUE);
 				if(newPlace.placeType.contains("capitalOfCountry")) myDart->actionCapitals_of_Countries->setVisible(TRUE);

cpp/results.cpp
@@ -72,7 +72,7 @@ resultWindow::resultWindow(dart *TDart, int PLayer, io *TIO, QDialog *parent) : 
 		else km=QString("%1").arg(myDart->qlScoreHistory[player][i].diffKm,0,'f',1);
 		
 		places+=QString(tr("<span style=\"color:rgb(%5,%6,%7)\">%1&nbsp;(%2&nbsp;km,&nbsp;%3&nbsp;P.,&nbsp;%4)</span>, "))
-		                .arg(myDart->qlCurrentTypePlaces[myDart->qlPlacesHistory[i]]->name)
+		                .arg(myDart->qlPlacesHistory[i]->name)
 		                .arg(km)
 		                .arg(myDart->qlScoreHistory[player][i].score)
 		                .arg(myDart->qlScoreHistory[player][i].mark,0,'f',1)

icons.qrc
@@ -9,4 +9,7 @@
         <file alias="player-time.png">icons/oxygen/player-time.png</file>
         <file alias="index.theme">icons/oxygen/index.theme</file>
     </qresource>
+    <qresource prefix="/icons">
+        <file alias="banner.png">icons/banner.png</file>
+    </qresource>
 </RCC>

icons/.directory
@@ -1,0 +1,4 @@
+[Dolphin]
+ShowPreview=true
+Timestamp=2011,9,10,19,7,25
+Version=2

commit 09984c91fd6396c6622d91f5915576d5b6f67396

fixed bug in training mode
* fixed training-bug introduced in fe3276ecaec2b31acf7e3f5f289768ceb1baa7ef (introduced history pointer), which breaks the revision-algorithm [thanks to git bisect]
* prevent modifymakefile from adding multiple -Wno-reorder

Makefile
@@ -1,6 +1,6 @@
 #############################################################################
 # Makefile for building: QeoDart
-# Generated by qmake (2.01a) (Qt 4.7.4) on: So. Nov 13 16:42:31 2011
+# Generated by qmake (2.01a) (Qt 4.7.4) on: Fr. Nov 18 19:01:28 2011
 # Project:  QeoDart.pro
 # Template: app
 # Command: /usr/bin/qmake -o Makefile QeoDart.pro
@@ -11,8 +11,8 @@
 CC            = gcc
 CXX           = g++
 DEFINES       = -DQT_WEBKIT -DQT_NO_DEBUG -DQT_XML_LIB -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED
-CFLAGS        = -pipe -O2 -Wall -W -Wno-reorder -D_REENTRANT -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder $(DEFINES)
-CXXFLAGS      = -pipe -O2 -Wall -W -Wno-reorder -D_REENTRANT -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder -Wno-reorder $(DEFINES)
+CFLAGS        = -pipe -O2 -Wall -W -D_REENTRANT -Wno-reorder $(DEFINES)
+CXXFLAGS      = -pipe -O2 -Wall -W -D_REENTRANT -Wno-reorder $(DEFINES)
 INCPATH       = -I/usr/share/qt4/mkspecs/linux-g++ -I. -I/usr/include/qt4/QtCore -I/usr/include/qt4/QtGui -I/usr/include/qt4/QtXml -I/usr/include/qt4 -I. -Icpp -I. -I.
 LINK          = g++
 LFLAGS        = -Wl,-O1

cpp/dart.cpp
@@ -1168,7 +1168,8 @@ void dart::vNextRound() {
 	
 	if(iGameMode==enTraining && iPlaceCount>=5) {
 		qDebug() << "Revise";
-		for(int i=0; i<qlScoreHistory[0].count() && pn==-1; i++) {
+		for(int i=0; i<qlScoreHistory[0].count() && ppn==NULL; i++) {
+		//for(int i=0; i<qlScoreHistory[0].count() && pn==-1; i++) {
 			qDebug() << qlScoreHistory[0][i].mark << qlTotalScores[0].mark;
 			if(qlScoreHistory[0][i].mark>=4 || (qlScoreHistory[0][i].mark>2 && qlScoreHistory[0][i].mark>qlTotalScores[0].mark) ) {
 				qDebug() << qlPlacesHistory[i]->name;
@@ -1183,12 +1184,12 @@ void dart::vNextRound() {
 				}
 			} //if (badscore)
 		} // for (scorehistory)
-		if(pn==-1) {
+		if(ppn==NULL) {
 			vResetForNewGame();
 		}
 	}
 	
-	if(pn<=-1 && ppn==NULL) {
+	if(ppn==NULL) {
 		int i=0;
 		do {
 			qDebug() << qlCurrentTypePlaces.count();

modifymakefile
@@ -1,3 +1,3 @@
 #!/bin/bash 
 # I do not know a better way to add Wno's
-cat Makefile | sed -e "s/D_REENTRANT/D_REENTRANT -Wno-reorder/g" > Makefile2 && mv Makefile2 Makefile
+cat Makefile | sed -e "s/D_REENTRANT -Wno-reorder/D_REENTRANT/g" | sed -e "s/D_REENTRANT -Wno-reorder/D_REENTRANT/g" | sed -e "s/D_REENTRANT/D_REENTRANT -Wno-reorder/g" > Makefile2 && mv Makefile2 Makefile