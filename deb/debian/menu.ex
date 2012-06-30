b9ba090e66d067b7b6b2fe1f2470bc6ad1bd0e18
gulp21 <support.gulp21@googlemail.com>
2012-06-30 11:19:57 +0200

    makedeb * makedeb bash script for creating debian package * support for translations installed in /usr/share/QeoDart/lang

---
+++ deb/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(qeodart-2.0):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="qeodart-2.0" command="/usr/bin/qeodart-2.0"
---
+++ deb/debian/qeodart-2.0.default.ex
@@ -0,0 +1,10 @@
+# Defaults for qeodart-2.0 initscript
+# sourced by /etc/init.d/qeodart-2.0
+# installed at /etc/default/qeodart-2.0 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
